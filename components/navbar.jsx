b94eff66e1748371791e5c0a1635be47dc748b77
David Adelwine Adumolga <adumolgaadelwine@gmail.com>
2024-08-31 21:22:43 +0000

    update

--- components/navbar.jsx
+++ components/navbar.jsx
@@ -5,12 +5,13 @@ import logo from '@/assets/images/logo-white.png';
 import profileDefault from '@/assets/images/profile.png';
 import Link from 'next/link';
 import {FaGoogle} from 'react-icons/fa';
-import {  usePathname } from 'next/navigation';
+import { usePathname } from 'next/navigation';
 const Navbar = () => {  
-  const [ismobileMenuOpen,setIsMobileMenuOpen]=useState(false)      
-  const [isProfileMenuOpen,setIsProfileMenuOpen]=useState(false)     
+  const [ismobileMenuOpen,setIsMobileMenuOpen]=useState(false)  ;    
+  const [isProfileMenuOpen,setIsProfileMenuOpen]=useState(false) ;   
+  const [isLoggedIn,setIsLoggedOut]=useState(false) ;
   
-  const pathname=usePathname;
+  const pathname=usePathname();
 
   return (
     <nav className="bg-blue-700 border-b border-blue-500">
@@ -73,22 +74,27 @@ const Navbar = () => {
               <Link
                 href="/properties"
                 className={`${
-                  pathname === '/properties' ? 'bg-black' : ''
-                }text-white  hover:bg-gray-900 hover:text-white rounded-md px-3 py-2`}
+                  pathname === '/properties' ? 'bg-black' : ''}
+                 text-white  hover:bg-gray-900 hover:text-white rounded-md px-3 py-2`}
                 >Properties
                 </Link >
+
+                { isLoggedIn && (
               <Link
                 href="/properties/add"
                 className={`${
-                  pathname === '/properties/add' ? 'bg-black' : ''
-                }text-white  hover:bg-gray-900 hover:text-white rounded-md px-3 py-2`}
+                  pathname === '/properties/add' ? 'bg-black' : ''}
+                   text-white  hover:bg-gray-900 hover:text-white rounded-md px-3 py-2`}
                 >Add Property
                 </Link >
+                )}
             </div>
           </div>
         </div>
 
         {/* <!-- Right Side Menu (Logged Out) --> */}
+        {
+        !isLoggedIn && (
         <div className="hidden md:block md:ml-6">
           <div className="flex items-center">
             <button
@@ -99,8 +105,12 @@ const Navbar = () => {
             </button>
           </div>
         </div>
+        )}
 
         {/* <!-- Right Side Menu (Logged In) --> */}
+
+        {
+          isLoggedIn && (
         <div
           className="absolute inset-y-0 right-0 flex items-center pr-2 md:static md:inset-auto md:ml-6 md:pr-0"
         >
@@ -193,6 +203,8 @@ const Navbar = () => {
             )}
           </div>
         </div>
+
+)}
       </div>
     </div>
 
@@ -202,25 +214,34 @@ const Navbar = () => {
       <div className="space-y-1 px-2 pb-3 pt-2">
         <Link
           href="/"
-          className="bg-black text-white block rounded-md px-3 py-2 text-base font-medium"
+          className={`${pathname ==='/' ? 'bg-black' :''}
+          text-white block rounded-md px-3 py-2 text-base font-medium` }
           >Home
           </Link>
         <Link
           href="/properties"
-          className="text-white block rounded-md px-3 py-2 text-base font-medium"
-          >Properties</Link
-        >
+          className={`${pathname ==='/properties' ? 'bg-black' :''}
+          text-white block rounded-md px-3 py-2 text-base font-medium` }
+          >Properties
+          </Link >
+        {
+        isLoggedIn && (
         <Link
           href="/properties/add"
-          className="text-white block rounded-md px-3 py-2 text-base font-medium"
-          >Add Property</Link
-        >
+          className={`${pathname ==='/properties/add' ? 'bg-black' :''}
+          text-white block rounded-md px-3 py-2 text-base font-medium` }
+          >Add Property
+          </Link >
+        )}
+          {
+            !isLoggedIn && (
         <button
           className="flex items-center text-white bg-gray-700 hover:bg-gray-900 hover:text-white rounded-md px-3 py-2 my-5"
         >
           <FaGoogle className='test-while mr-2'/>
           <span>Login or Register</span>
         </button>
+        )}
       </div>
     </div>
     )}
